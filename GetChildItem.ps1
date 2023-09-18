e7dd19a385ba43751bda5993ffd6e259411f801e
a-534178 <52885611+a-534178@users.noreply.github.com>
2023-09-18 18:03:08 +0300

    Delete GetChildItem.ps1

--- GetChildItem.ps1
+++
@@ -1,4 +0,0 @@
-﻿#Формирование списка файлов с путями из папки с множеством вложенных папок
-$UpdatePath = "C:\temp\W2k12patches"
-Get-ChildItem -Path $UpdatePath -Recurse -Include *.cab* | ForEach-Object -Process {
-                                                           'DISM.exe /Online /Add-Package /PackagePath:'+$_.Name | Out-File -FilePath C:\temp\updatelist.txt -Append}
