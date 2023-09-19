515c4d7f850e1ec708e7e1fa00d694a052f2eccd
alexlyshov <aleksey.lyshov@gmail.com>
2023-09-19 15:46:50 +0300

    Add CopyFiles.ps1 script

---
+++ CopyFiles.ps1
@@ -0,0 +1,7 @@
+﻿# Скрипт рекурсивно проходит по каталогу $UpdatePath
+# Выполняет копирование всех найденных файлов с расширением .cab в -Destination
+$UpdatePath = "C:\Users\a534178.INT\Documents\Backup\Documents\PS Scripts\Test"
+Get-ChildItem -Path $UpdatePath -Recurse -Include *.cab* | ForEach-Object -Process {
+    $path = $_.DirectoryName+'\'+$_.Name
+   # $path}
+    Copy-Item $path -Destination ''}
