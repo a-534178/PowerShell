2595312946ddc8e11aa6f471588dc8a021ccf06f
alexlyshov <aleksey.lyshov@gmail.com>
2023-09-26 22:42:43 +0300

    Add new script UserPlusGroupMembership,ps1

---
+++ UserPlusGroupMembership.ps1
@@ -0,0 +1,27 @@
+﻿<#
+Скрипт формирует отчет в GridView или в csv файл, где видно какой пользователь в каких группах состоит.
+В качестве входных парметров мы задаем OU из которых мы будем брать пользователей и производить анализ 
+их членства в группах AD.
+#>
+$users = Get-ADUser -SearchBase "" -SearchScope OneLevel -Filter * -Properties *
+$users.count
+$result = @()
+
+foreach ($user in $users){
+    
+    
+    
+    $groups = Get-ADPrincipalGroupMembership $user | select name
+
+    $res = [PSCustomObject]@{
+        SamAccountName = $user.SamAccountName
+        DisplayName = $user.DisplayName
+        Groups = (@($groups.Name) -join ',')
+        }
+
+        $result+=$res
+        $res
+       
+    }
+$result | Out-GridView
+#$result #| Export-Csv "C:\Temp" -Encoding UTF8 -NoTypeInformation
